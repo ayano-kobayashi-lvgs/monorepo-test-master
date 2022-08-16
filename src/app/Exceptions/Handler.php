77bd34c4f4e8cd7cd4b40df8ab3ae34f37395992
小林　菖乃 <ayano.kobayashi@PC-3488.local>
2022-08-16 11:43:11 +0900

    認可 レビュー修正

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -12,9 +15,7 @@ class Handler extends ExceptionHandler
      *
      * @var array<int, class-string<Throwable>>
      */
-    protected $dontReport = [
-        //
-    ];
+    protected $dontReport = [];
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
@@ -32,10 +33,55 @@ class Handler extends ExceptionHandler
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+    }
+
+    /**
+     * report
+     */
+    public function report(Throwable $e): void
+    {
+        parent::report($e);
+    }
+
+    /**
+     * render
+     */
+    public function render($request, Throwable $e): Response
+    {
+        return parent::render($request, $e);
+    }
+
+    /**
+     * HTTP以外の例外発生時の処理
+     */
+    protected function prepareResponse($request, Throwable $e): Response
+    {
+        // HTTPでない例外が起これば、HTTP例外の500に変更する
+        if (!$this->isHttpException($e) && !config('app.debug')) {
+            $e = new HttpException(500, $e->getMessage(), $e);
+        }
+
+        return parent::prepareResponse($request, $e);
+    }
+
+    /**
+     * HTTP例外発生時の処理
+     */
+    protected function renderHttpException(HttpExceptionInterface $e): Response
+    {
+        $status = $e->getStatusCode();
+
+        return response()->view(
+            'errors.common',
+            [
+                'exception' => $e,
+                'message' => $e->getMessage(),
+                'status_code' => $status,
+            ],
+            $status,
+            $e->getHeaders()
+        );
     }
 }
--- src/resources/views/errors/common.blade.php
+++ src/resources/views/errors/common.blade.php
@@ -1,21 +1,23 @@
 @extends('layouts.app')
-@section('title', 'アクセスエラー')
+@section('title', 'エラー')
 @section('css')
-    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/style.css') }}">
 @endsection
+
 <body>
     <div class="c-section">
         <div class="c-section__inner">
             <div class="c-section__box">
                 <div class="c-section__boxInner p-register__boxInner">
                     <h1 class="c-section__title p-regist__title">
-                        アクセスエラー
+                        {{ trans("errors.${status_code}.title") }}
                     </h1>
-                    <p class="c-section__body">アクセス権限がありません。</p>
+                    <p class="c-section__body">
+                        {{ trans("errors.${status_code}.message") }}
+                    </p>
                     <div class="c-section__btnWrap">
-                        <p
-                            class="c-btn c-section__btn c-section__btn--small">
-                            <a href="{{ route('todos.index') }}">Todo一覧に戻る</a>
+                        <p class="c-btn c-section__btn c-section__btn--small">
+                            <a href="{{ route('auth.login') }}">ログインへ</a>
                         </p>
                     </div>
                 </div>
